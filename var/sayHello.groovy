f254cedbadde34a9fb27f929dc148cbfed4847c9
modernlearn <115264644+modernlearn@users.noreply.github.com>
2022-11-03 13:57:06 -0600

    Update sayHello.groovy

--- var/sayHello.groovy
+++ var/sayHello.groovy
@@ -1,10 +1,5 @@
 #!/usr/bin/env groovy
 
-def someFunc(String text){
-    echo text
-    text
-}
-def someFunc2(String text2){
-    echo text2
-    text2
+def call(String name = 'human') {
+    echo "Hello, ${name}."
 }
