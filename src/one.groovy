28e9e3dd650026be4ce138bfe487617e43d3b3c6
modernlearn <115264644+modernlearn@users.noreply.github.com>
2022-11-06 12:58:30 -0600

    Update one.groovy

--- src/one.groovy
+++ src/one.groovy
@@ -1,4 +1,5 @@
 class one {
    static String foo = "bar"
    checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: src]], branches: [[name: 'refs/tags/3.6.1']]], poll: false
+   println "here we are testing"
 }
