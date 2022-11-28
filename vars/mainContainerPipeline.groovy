1f51a995f60e64c0f6fc884ad9da786f6b18fcde
modernlearn <115264644+modernlearn@users.noreply.github.com>
2022-11-28 16:58:33 -0600

    Update mainContainerPipeline.groovy

--- vars/mainContainerPipeline.groovy
+++ vars/mainContainerPipeline.groovy
@@ -2,11 +2,9 @@
 import  groovy.transform.Field
 
 
-def call(Map propertyInfo) {
-  echo "Hello, ${propertyInfo.label}."
-    checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: 'https://github.com/modernlearn/gitos.git']], branches: [[name: 'refs/tags/0935']]], poll: false
-   sh "ls -l > commandResult"
-   result = readFile('commandResult').trim()
+def call( {
+  echo "Hello "
+
   println "here we are testing"
 }
 
